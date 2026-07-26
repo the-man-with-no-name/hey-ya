11fae671e7915b167b12604e5ff6184112131980
the-man-with-no-name <64222379+the-man-with-no-name@users.noreply.github.com>
2026-07-25 21:00:14 -0700

    Further prompt engineering.

--- hey.zsh
+++ hey.zsh
@@ -25,42 +25,22 @@ function _think() {
 function _hey_logic() {
     local user_prompt="$1"
 
-    local system_prompt="You are a zsh assistant on a Mac computer.
-    Return ONLY the shell command to execute. No markdown, no explanations, just the raw code.
-    The command should ONLY be a single line. DO NOT return a command spanning multiple lines with newline characters.
-    This output is displayed in a terminal environment, format the output as such.
-    Do not include conversational intro or outro.
-    Wrap lines at 80 characters to fit standard terminal windows.
-    Strip markdown blocks and markdown headers, do not use triple backticks or markdown fences or headings.
-    Ensure you use a backslash to escape all necessary characters such as:
-        newlines,
-        spaces in file or directory names,
-        single quotes, double quotes, and backslash,
-        brackets,
-        braces,
-        parantheses,
-        control characters,
-        and escape characters."
+    local system_prompt="You are a Z Shell assistant on a Macintosh system.
+    You will be asked to determine how to perform a certain task in a Z Shell environment.
+    Return ONLY the shell command to execute. 
+    Do not format markdown, no explanations, just the raw code.
+    The command must be ONE line of code."
 
-    local system_prompt_summary="You are a zsh assistant on a Mac computer.
-    Return a summary of what the user is asking in a short, simple sentence.
-    DO NOT include the shell command itself as part of your response, plain english only.
-    Start the summary sentence with the verb and do not say YOU or THE USER, keep it simple.
-    If the command is destructive, start the summary with DESTRUCTIVE.
-    If the command takes large amounts of time, start the summary with SLOW.
+    local system_prompt_summary="You are a Z Shell assistant on a Macintosh system.
+    You will be provided with a question of how to perform a command in Z Shell on a Macintosh system.
+    Return a summary of what the user is asking in ONE short, simple sentence.
+    DO NOT include the shell command itself as part of your response.
+    Start the summary sentence with a verb and do not say YOU or THE USER, and keep the sentence simple.
+    If the command is destructive, begin the summary with 'DESTRUCTIVE! '.
+    If the command takes large amounts of time, begin the summary with 'SLOW! '.
     This output is displayed in a terminal environment, format the output as such.
     Do not include conversational intro or outro.
-    Wrap lines at 80 characters to fit standard terminal windows.
-    Strip markdown blocks and markdown headers, do not use triple backticks or markdown fences or headings.
-    Ensure you use a backslash to escape all necessary characters such as:
-        newlines,
-        spaces in file or directory names,
-        single quotes, double quotes, and backslash,
-        brackets,
-        braces,
-        parantheses,
-        control characters,
-        and escape characters."
+    Wrap lines at 80 characters to fit standard terminal windows."
 
     local error_color="#D55E00"
     local think_color="#FDF1A9"
@@ -118,10 +98,19 @@ function _hey_info() {
 
     local debug="0"
 
+    local system_prompt="You are a Z Shell knowledge expert on a Macintosh system.
+    You will be provided with a shell command the result of running that command.
+    Provide information about what the shell command does and summarize what the provided result means.
+    Do not call this result explanation a summary or review.
+    This output is displayed in a terminal environment, format the output as such.
+    Do not include conversational intro or outro.
+    Wrap lines at 80 characters to fit standard terminal windows.
+    Strip markdown blocks and markdown headers, do not use triple backticks or markdown fences or headings."
+
     if [[ $1 =~ ^[[:space:]]*$ ]]; then
         local last_command=$(history | tail -n 1)
         local clean_last_command="${last_command##[0-9 ]#}"
-        local last_command_result=$(eval "$clean_last_command")
+        local last_command_result=$(eval "$clean_last_command" 2>/dev/null)
 
         if [[ $debug == 1 ]]; then
             print -Pn "%F{$debug_color}get info about: last command%f\n"
@@ -131,26 +120,6 @@ function _hey_info() {
 
         # Print thinking message
         print -Pn "%F{$think_color}Thinking...%f\r"
-
-        local system_prompt="You are a zsh knowledge expert on a Mac computer.
-        Your only job is to provide information about shell commands and summarize their results.
-        Explain the command in plain language and, if you do not know,
-        direct the user to man or documentation where they can find information.
-        Provide a brief explanation of how the result of the command is interpreted.
-        Do not call this result explanation a summary or review.
-        This output is displayed in a terminal environment, format the output as such.
-        Do not include conversational intro or outro.
-        Wrap lines at 80 characters to fit standard terminal windows.
-        Strip markdown blocks and markdown headers, do not use triple backticks or markdown fences or headings.
-        Ensure you use a backslash to escape all necessary characters such as:
-            newlines,
-            spaces in file or directory names,
-            single quotes, double quotes, and backslash,
-            brackets,
-            braces,
-            parantheses,
-            control characters,
-            and escape characters."
 
         local user_prompt="The last command the user ran is: $last_command
         The result of this last command was: $last_command_result
@@ -172,22 +141,6 @@ function _hey_info() {
         local clean_text=$(printf '%s\n' "$response" | jq -r ".response")
         print -Pn "%F{$info_color}$clean_text%f\n"
     else
-        local system_prompt="You are a zsh knowledge expert on a Mac computer.
-        Your only job is to provide information about what the user is asking.
-        This output is displayed in a terminal environment, format the output as such.
-        Do not include conversational intro or outro.
-        Wrap lines at 80 characters to fit standard terminal windows.
-        Strip markdown blocks and markdown headers, do not use triple backticks or markdown fences or headings.
-        Ensure you use a backslash to escape all necessary characters such as:
-            newlines,
-            spaces in file or directory names,
-            single quotes, double quotes, and backslash,
-            brackets,
-            braces,
-            parantheses,
-            control characters,
-            and escape characters."
-
         # Print thinking message
         print -Pn "%F{$think_color}Thinking...%f\r"
 
